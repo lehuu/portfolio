8b22f7555a77576db71da44aef10339e7a067dc7
Phuoc Le <phuoc.le@tum.de>
2022-02-09 19:13:14 +0100

    🐛 fix missing gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
+const productionPlugins =
+  process.env.NODE_ENV === 'production' ? ['gatsby-plugin-no-sourcemaps'] : [];
+
 module.exports = {
   siteMetadata: {
-    title: 'portfolio',
-    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Phuoc Le',
+    siteUrl: 'https://www.bytecruncher.com',
   },
   plugins: [
+    ...productionPlugins,
     'gatsby-plugin-styled-components',
     'gatsby-plugin-typescript',
     'gatsby-plugin-image',
