66c8b94681ac14373b5c45b445e242555e221119
Phuoc Le <phuoc.le@tum.de>
2022-02-16 13:46:58 +0100

    🔧 fix tslint errors

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,25 +1,25 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import { ThemeProvider } from 'styled-components';
-import { darkTheme, GlobalStyle, lightTheme } from '../styles';
+import { GlobalStyle, lightTheme } from '../styles';
 
 interface LayoutProps {
   title: string;
 }
 
-export const Layout: React.FunctionComponent<LayoutProps> = ({ title, children }) => {
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
-      <Helmet>
-        <title>{title}</title>
-        <html lang="en" />
-      </Helmet>
-      <header>
-        <nav>dummy nav component</nav>
-      </header>
-      <main>{children}</main>
-      <footer>dummy footer</footer>
-    </ThemeProvider>
-  );
-};
+const Layout: React.FunctionComponent<LayoutProps> = ({ title, children }) => (
+  <ThemeProvider theme={lightTheme}>
+    <GlobalStyle />
+    <Helmet>
+      <title>{title}</title>
+      <html lang="en" />
+    </Helmet>
+    <header>
+      <nav>dummy nav component</nav>
+    </header>
+    <main>{children}</main>
+    <footer>dummy footer</footer>
+  </ThemeProvider>
+);
+
+export default Layout;
