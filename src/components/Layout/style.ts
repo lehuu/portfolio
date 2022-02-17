d101761b02eb871eebdc645aa71025ee29dadedf
Phuoc Le <phuoc.le@tum.de>
2022-02-17 23:27:41 +0100

    ✨ add main content style

--- src/components/Layout/style.ts
+++ src/components/Layout/style.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
-const FlexContainer = styled.div``;
+const Main = styled.main`
+  flex: 1 0 auto;
+  padding: ${({ theme }) => `${theme.gaps.xl} ${theme.gaps.l}`};
+`;
 
-export default { FlexContainer };
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+`;
+
+export default { FlexContainer, Main };
