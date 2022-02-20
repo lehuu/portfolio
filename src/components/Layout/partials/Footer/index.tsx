4d7588c24d2ee13949bd83fa1c31b369206d902b
Phuoc Le <phuoc.le@tum.de>
2022-02-20 12:11:24 +0100

    ♻️ refactor footer with metadata

--- src/components/Layout/partials/Footer/index.tsx
+++ src/components/Layout/partials/Footer/index.tsx
@@ -1,24 +1,32 @@
 import * as React from 'react';
+import { useSiteMetadata } from '@hooks';
 import Styled from './style';
 
-const Footer: React.FunctionComponent = () => (
-  <Styled.Footer>
-    <div>
-      <Styled.InternalLink to="/imprint">Imprint</Styled.InternalLink>
-    </div>
-    <div>
-      <Styled.NoBreakSpan>© 2022 Phuoc Le.&nbsp;</Styled.NoBreakSpan>
-      <Styled.NoBreakSpan>All rights reserved.</Styled.NoBreakSpan>
-    </div>
-    <div>
-      <Styled.NoBreakSpan>Inspired by&nbsp;</Styled.NoBreakSpan>
-      <Styled.NoBreakSpan>
-        <Styled.ExternalLink href="https://brittanychiang.com/" target="_blank">
-          Brittany Chiang
-        </Styled.ExternalLink>
-      </Styled.NoBreakSpan>
-    </div>
-  </Styled.Footer>
-);
+const Footer: React.FunctionComponent = () => {
+  const { title } = useSiteMetadata();
+  const year = new Date().getFullYear();
+
+  return (
+    <Styled.Footer>
+      <div>
+        <Styled.InternalLink to="/imprint">Imprint</Styled.InternalLink>
+      </div>
+      <div>
+        <Styled.NoBreakSpan>
+          © {year} {title}.&nbsp;
+        </Styled.NoBreakSpan>
+        <Styled.NoBreakSpan>All rights reserved.</Styled.NoBreakSpan>
+      </div>
+      <div>
+        <Styled.NoBreakSpan>Inspired by&nbsp;</Styled.NoBreakSpan>
+        <Styled.NoBreakSpan>
+          <Styled.ExternalLink href="https://brittanychiang.com/" target="_blank">
+            Brittany Chiang
+          </Styled.ExternalLink>
+        </Styled.NoBreakSpan>
+      </div>
+    </Styled.Footer>
+  );
+};
 
 export default Footer;
