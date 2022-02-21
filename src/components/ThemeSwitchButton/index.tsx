5582e8cc3f1ae74e0dd7a363a493d024d5b21a96
Phuoc Le <phuoc.le@tum.de>
2022-02-21 02:47:29 +0100

    ♻️ improve accessibility

--- src/components/ThemeSwitchButton/index.tsx
+++ src/components/ThemeSwitchButton/index.tsx
@@ -6,7 +6,7 @@ const ThemeSwitchButton: React.FunctionComponent = () => {
   const { isDarkMode, toggle } = useTheme();
 
   return (
-    <Styled.Button type="button" onClick={toggle}>
+    <Styled.Button type="button" onClick={toggle} aria-label="switch theme">
       <Styled.SunIcon $isVisible={!isDarkMode} />
       <Styled.MoonIcon $isVisible={isDarkMode} />
     </Styled.Button>
