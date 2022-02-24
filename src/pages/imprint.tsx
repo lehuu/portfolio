f70de836220be848a9222d7275dfaf10ccada8b2
Phuoc Le <phuoc.le@tum.de>
2022-02-24 02:58:24 +0100

    ♻️ change React import

--- src/pages/imprint.tsx
+++ src/pages/imprint.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { Layout } from '@components';
 import styled from '@emotion/styled';
 import { contentWidth, mixinTransition } from '@styles';
