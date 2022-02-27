fcbad657bae576c05372cc4142419ccd58ca4b2f
Phuoc Le <phuoc.le@tum.de>
2022-02-27 18:53:03 +0100

    ✨ add about content

--- src/components/Sections/About/index.tsx
+++ src/components/Sections/About/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { SectionHeader } from '@components';
+import SectionHeader from '@components/SectionHeader';
+import { graphql, useStaticQuery } from 'gatsby';
+import Styled from './style';
 
-const About: React.FunctionComponent = () => (
-  <section style={{ height: '300px' }}>
-    <SectionHeader title="About Me" />
-    About
-  </section>
-);
+const ABOUT_QUERY = graphql`
+  {
+    markdownRemark(fileAbsolutePath: { regex: "/content/about/index.md/" }) {
+      html
+    }
+  }
+`;
+
+const About: React.FunctionComponent = () => {
+  const {
+    markdownRemark: { html },
+  } = useStaticQuery(ABOUT_QUERY);
+
+  return (
+    <section>
+      <SectionHeader title="About Me" />
+      {/* eslint-disable-next-line react/no-danger */}
+      <Styled.TextContainer dangerouslySetInnerHTML={{ __html: html }} />
+    </section>
+  );
+};
 
 export default About;
--- src/components/Sections/index.ts
+++ src/components/Sections/index.ts
@@ -1,3 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as IntroSection } from './Intro';
 export { default as AboutSection } from './About';
