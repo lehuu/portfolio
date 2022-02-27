32d5aa9b73ab7fec96dccb14732d6ec0f4a55638
Phuoc Le <phuoc.le@tum.de>
2022-02-27 20:22:55 +0100

    ✨ add contact section with dummy content

---
+++ src/components/Sections/Contact/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import SectionHeader from '@components/SectionHeader';
+import { graphql, useStaticQuery } from 'gatsby';
+import Styled from './style';
+
+const CONTACT_QUERY = graphql`
+  {
+    markdownRemark(fileAbsolutePath: { regex: "/content/contact/index.md/" }) {
+      html
+    }
+  }
+`;
+
+const Contact: React.FunctionComponent = () => {
+  const {
+    markdownRemark: { html },
+  } = useStaticQuery(CONTACT_QUERY);
+
+  return (
+    <section>
+      <SectionHeader title="Contact" />
+      {/* eslint-disable-next-line react/no-danger */}
+      <Styled.TextContainer dangerouslySetInnerHTML={{ __html: html }} />
+    </section>
+  );
+};
+
+export default Contact;
