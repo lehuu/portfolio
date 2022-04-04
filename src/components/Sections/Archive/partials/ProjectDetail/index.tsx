5df8d91dba699ecdcd1cf7e840bd3cb8c9a25ef6
Phuoc Le <phuoc.le92@gmail.com>
2022-04-05 01:54:20 +0200

    ✨ add company and techstack to archive detail

--- src/components/Sections/Archive/partials/ProjectDetail/index.tsx
+++ src/components/Sections/Archive/partials/ProjectDetail/index.tsx
@@ -3,18 +3,36 @@ import Img, { FluidObject } from 'gatsby-image';
 import Styled from './style';
 
 interface ProjectDetailProps {
+  company?: string;
   description?: string;
+  techstack?: string[];
   image: FluidObject | FluidObject[];
 }
 
-const ProjectDetail: React.FunctionComponent<ProjectDetailProps> = ({ image, description }) => (
+const ProjectDetail: React.FunctionComponent<ProjectDetailProps> = ({
+  company,
+  image,
+  description,
+  techstack,
+}) => (
   <Styled.DetailsContainer>
     {description ? (
       <>
         <Styled.ImageContainer>
           <Img fluid={image} alt="Project Image" />
         </Styled.ImageContainer>
-        <Styled.TextContainer dangerouslySetInnerHTML={{ __html: description }} />
+        <Styled.TextContainer>
+          {company && <Styled.CompanyContainer>Made at: {company}</Styled.CompanyContainer>}
+          {/* eslint-disable-next-line react/no-danger */}
+          <div dangerouslySetInnerHTML={{ __html: description }} />
+          {techstack && (
+            <Styled.TechList>
+              {techstack.map((tech) => (
+                <li key={tech}>{tech}</li>
+              ))}
+            </Styled.TechList>
+          )}
+        </Styled.TextContainer>
       </>
     ) : (
       <Styled.NoDetailsContainer>No description available</Styled.NoDetailsContainer>
